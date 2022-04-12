a600345b042007b2f9bc1999c11194372ffa2f53
Diarmuid O Connor <doconnor@wit.ie>
2022-04-12 17:27:28 +0100

    Deployed DynamoDB table

--- lib/cdk-three-tier-serverless-stack.ts
+++ lib/cdk-three-tier-serverless-stack.ts
@@ -1,16 +1,18 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkThreeTierServerlessStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkThreeTierServerlessQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const table = new Table(this, 'NotesTable', {
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: 'pk', type: AttributeType.STRING },
+      removalPolicy: RemovalPolicy.DESTROY,
+      sortKey: { name: 'sk', type: AttributeType.STRING },
+      tableName: 'NotesTable',
+    });
   }
+
 }
