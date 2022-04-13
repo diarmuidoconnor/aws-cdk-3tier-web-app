24563c5bfb77eb4f257ecb1d5112693ca6e32d4e
Diarmuid O Connor <doconnor@wit.ie>
2022-04-13 13:38:59 +0100

    Deployed API Gateway

--- lib/cdk-three-tier-serverless-stack.ts
+++ lib/cdk-three-tier-serverless-stack.ts
@@ -1,10 +1,15 @@
-import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Architecture } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { Construct } from 'constructs';
-
+import {
+  CorsHttpMethod,
+  HttpApi,
+  HttpMethod,
+} from '@aws-cdk/aws-apigatewayv2-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 export class CdkThreeTierServerlessStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -32,5 +37,37 @@ export class CdkThreeTierServerlessStack extends Stack {
     table.grantReadData(readFunction);
 
     table.grantWriteData(writeFunction);
+
+    const api = new HttpApi(this, 'NotesApi', {
+      corsPreflight: {
+        allowHeaders: ['Content-Type'],
+        allowMethods: [CorsHttpMethod.GET, CorsHttpMethod.POST],
+        allowOrigins: ['*'],
+      },
+    });
+
+    const readIntegration = new HttpLambdaIntegration(
+      'ReadIntegration',
+      readFunction
+    );
+
+    const writeIntegration = new HttpLambdaIntegration(
+      'WriteIntegration',
+      writeFunction
+    );
+
+    api.addRoutes({
+      integration: readIntegration,
+      methods: [HttpMethod.GET],
+      path: '/notes',
+    });
+
+    api.addRoutes({
+      integration: writeIntegration,
+      methods: [HttpMethod.POST],
+      path: '/notes',
+    });
+    new CfnOutput(this, 'HttpApiUrl', { value: api.apiEndpoint });
+
   }
 }
